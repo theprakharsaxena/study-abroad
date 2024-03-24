b09fa9c50f47d53ee6858af3698d483f26d1c448
theprakharsaxena <prakharsaxena5125@gmail.com>
2024-03-24 11:50:42 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Horizonwings Overseas Education",
   description: "Where Your International Academic Aspirations Take Wing!",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +21,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
+      <link rel="manifest" href="images/site.webmanifest" />
       <body className={inter.className}>{children}</body>
     </html>
   );
