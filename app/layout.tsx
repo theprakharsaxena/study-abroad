7bf72e1707d0bdc292f6ae6e563792582b30f72a
theprakharsaxena <prakharsaxena5125@gmail.com>
2024-03-27 06:57:50 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,11 @@ export const metadata: Metadata = {
   title: "Horizonwings Overseas Education",
   description: "Where Your International Academic Aspirations Take Wing!",
   icons: {
-    icon: ["/favicon.ico?v=4"],
-    apple: ["/apple-touch-icon.png?v=4"],
-    shortcut: ["/apple-touch-icon.png"],
+    icon: ["/images/favicon.ico?v=1"],
+    apple: ["/images/apple-touch-icon.png?v=1"],
+    shortcut: ["/images/apple-touch-icon.png"],
   },
+  manifest: "/images/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -21,24 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/images/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/images/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/images/favicon-16x16.png"
-      />
-      <link rel="manifest" href="images/site.webmanifest" />
       <body className={inter.className}>{children}</body>
     </html>
   );
